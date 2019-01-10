3dccebd23f860fe4df66a2531f7334f101508879
Roger <rogerlbrown@gmail.com>
2019-01-10 06:08:13 -0600

    setting up project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # python-learning
+
+## Resources
+
+* [https://thepythonguru.com/datatype-varibles/](thepythonguru.com)
+* [https://docs.python.org/2/tutorial/](python.org)
---
+++ hello.py
@@ -0,0 +1,18 @@
+
+#comments
+msg = "Hello World"
+print(msg)
+
+#simultaneous assignments
+x = 1
+y = 2
+print(x,y)
+y,x = x,y # <-
+print(x,y)
+
+#python has 5 standard data types
+# Numbers, strings, lists, tuple, dictionary, boolean 
+
+# input from console input()
+name = input("What is your name: ")
+print("Hello,", name)
