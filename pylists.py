036384191b29a583c8fc010edfbf84d6e76d12f4
Roger Brown <rogerlbrown@gmail.com>
2019-01-14 13:28:57 +0000

    playing with mapping functions

--- pylists.py
+++ pylists.py
@@ -2,7 +2,7 @@
 # https://docs.python.org/3.7/contents.html
 # https://docs.python.org/3.7/tutorial/datastructures.html
 
-
+line = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 list_names = [{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'},{'name': 'Homer'},{'name': 'Marge'}]
 
 print("# get one item from the list")
@@ -14,3 +14,16 @@ def namelist(names):
 
 a = namelist(list_names)
 print(a)
+
+print(line)
+#[print(x) for x in range(4) if print('checking') or True]
+
+def title_case(title, minor_words):
+    t = title.lower()
+    m = minor_words.lower()
+    result = map(lambda x, y: if x not in y: x.capitalize(), title, minor_words)
+    return result
+
+print("KINGS".capitalize())
+print("a clash of KINGS".lower().split(" "))
+title_case('a clash of KINGS', 'a an the of'), 'A Clash of Kings'
